321bdf8612b52f1005a69c21e280d2db9fe68494
Federico Feroldi <federico@teamdigitale.governo.it>
2019-08-21 17:27:19 +0200

    Adds GetServicesForRecipient function

---
+++ GetServicesForRecipient/index.ts
@@ -0,0 +1,70 @@
+import { Context } from "@azure/functions";
+
+import * as cors from "cors";
+import * as express from "express";
+import * as winston from "winston";
+
+import { DocumentClient as DocumentDBClient } from "documentdb";
+
+import {
+  SENDER_SERVICE_COLLECTION_NAME,
+  SenderServiceModel
+} from "io-functions-commons/dist/src/models/sender_service";
+import * as documentDbUtils from "io-functions-commons/dist/src/utils/documentdb";
+import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
+import { secureExpressApp } from "io-functions-commons/dist/src/utils/express";
+import { AzureContextTransport } from "io-functions-commons/dist/src/utils/logging";
+import { setAppContext } from "io-functions-commons/dist/src/utils/middlewares/context_middleware";
+
+import createAzureFunctionHandler from "io-functions-express/dist/src/createAzureFunctionsHandler";
+
+import { GetServicesForRecipient } from "./handler";
+
+// Setup Express
+const app = express();
+secureExpressApp(app);
+
+// Set up CORS (free access to the API from browser clients)
+app.use(cors());
+
+const cosmosDbUri = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_URI");
+const cosmosDbKey = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_KEY");
+const cosmosDbName = getRequiredStringEnv("COSMOSDB_NAME");
+
+const documentDbDatabaseUrl = documentDbUtils.getDatabaseUri(cosmosDbName);
+const senderServicesCollectionUrl = documentDbUtils.getCollectionUri(
+  documentDbDatabaseUrl,
+  SENDER_SERVICE_COLLECTION_NAME
+);
+
+const documentClient = new DocumentDBClient(cosmosDbUri, {
+  masterKey: cosmosDbKey
+});
+
+const senderServiceModel = new SenderServiceModel(
+  documentClient,
+  senderServicesCollectionUrl
+);
+
+app.get(
+  "/api/v1/profiles/:fiscalcode/sender-services",
+  GetServicesForRecipient(senderServiceModel)
+);
+
+const azureFunctionHandler = createAzureFunctionHandler(app);
+
+// tslint:disable-next-line: no-let
+let logger: Context["log"] | undefined;
+const contextTransport = new AzureContextTransport(() => logger, {
+  level: "debug"
+});
+winston.add(contextTransport);
+
+// Binds the express app to an Azure Function handler
+function httpStart(context: Context): void {
+  logger = context.log;
+  setAppContext(app, context);
+  azureFunctionHandler(context);
+}
+
+export default httpStart;
