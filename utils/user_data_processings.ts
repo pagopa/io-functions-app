2a17aae80808f7791f551c1c8f40641ef30fa4b2
Danilo Spinelli <gunzip@users.noreply.github.com>
2020-03-05 14:35:03 +0100

    [#171211343]-user data processing api (#42)

--- utils/user_data_processings.ts
+++ utils/user_data_processings.ts
@@ -2,7 +2,7 @@ import { UserDataProcessing } from "io-functions-commons/dist/generated/definiti
 import { RetrievedUserDataProcessing } from "io-functions-commons/dist/src/models/user_data_processing";
 
 /**
- * Converts a UserDataProcessingProfile model to an UserDataProcessing
+ * Converts a RetrievedUserDataProcessing model to an UserDataProcessing
  */
 export function toUserDataProcessingApi(
   userDataProcessing: RetrievedUserDataProcessing
@@ -10,7 +10,6 @@ export function toUserDataProcessingApi(
   return {
     choice: userDataProcessing.choice,
     created_at: userDataProcessing.createdAt,
-    fiscal_code: userDataProcessing.fiscalCode,
     status: userDataProcessing.status,
     updated_at: userDataProcessing.updatedAt,
     version: userDataProcessing.version
