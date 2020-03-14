61f5164ad8eb065847986bbb351169ebdb6116ab
Francesco Persico <francescopersico@gmail.com>
2020-03-14 10:16:38 +0100

    [#171800016] Use appsettings instead of connectionstrings (#46)

--- GetMessage/index.ts
+++ GetMessage/index.ts
@@ -26,8 +26,8 @@ import { GetMessage } from "./handler";
 const app = express();
 secureExpressApp(app);
 
-const cosmosDbUri = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_URI");
-const cosmosDbKey = getRequiredStringEnv("CUSTOMCONNSTR_COSMOSDB_KEY");
+const cosmosDbUri = getRequiredStringEnv("COSMOSDB_URI");
+const cosmosDbKey = getRequiredStringEnv("COSMOSDB_KEY");
 const cosmosDbName = getRequiredStringEnv("COSMOSDB_NAME");
 const messageContainerName = getRequiredStringEnv("MESSAGE_CONTAINER_NAME");
 
