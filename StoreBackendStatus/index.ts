3d5dcdab171772812d8441ca175cd7e90c937c6e
Danilo Spinelli <gunzip@users.noreply.github.com>
2020-03-16 16:36:01 +0100

    [#171179889] fix StoreBackendStatus (#52)

--- StoreBackendStatus/index.ts
+++ StoreBackendStatus/index.ts
@@ -10,23 +10,25 @@ import { get } from "superagent";
 const statusEndpointUrl = getRequiredStringEnv("STATUS_ENDPOINT_URL");
 const refreshIntervalMs = getRequiredStringEnv("STATUS_REFRESH_INTERVAL_MS");
 
-type IStoreBackendStatusHandler = (context: Context) => Promise<void>;
+export async function index(context: Context): Promise<void> {
+  // fetch backend info endpoint
+  const backendInfoJson = await get(statusEndpointUrl)
+    .timeout(10000)
+    .accept("json")
+    .then(res => res.body);
 
-export function index(): IStoreBackendStatusHandler {
-  return async context => {
-    // fetch backend info endpoint
-    const backendInfoJson = await get(statusEndpointUrl)
-      .timeout(10000)
-      .accept("json")
-      .then(res => res.body);
+  context.log.info(
+    "StoreBackendStatus|BACKEND_ENDPOINT=%s|STATUS=%s",
+    statusEndpointUrl,
+    JSON.stringify(backendInfoJson)
+  );
 
-    // store the json into the blob storage
-    // tslint:disable-next-line:no-object-mutation
-    context.bindings.backendStatus = {
-      ...backendInfoJson,
-      last_update: new Date().toISOString(),
-      refresh_interval: refreshIntervalMs
-    };
+  // store the json into the blob storage
+  // tslint:disable-next-line:no-object-mutation
+  context.bindings.backendStatus = {
+    ...backendInfoJson,
+    last_update: new Date().toISOString(),
+    refresh_interval: refreshIntervalMs
   };
 }
 
