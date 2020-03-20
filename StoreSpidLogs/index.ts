516524483eef48ffcd09023ed5232d806c945ef2
AleDore <alessio85.dore@gmail.com>
2020-03-20 16:32:45 +0100

    azure functions trigger bindings restored

--- StoreSpidLogs/index.ts
+++ StoreSpidLogs/index.ts
@@ -1,33 +1,40 @@
 import { Context } from "@azure/functions";
-import { createBlobService } from "azure-storage";
-import * as azureStorage from "azure-storage";
-import { format } from "date-fns";
+import { isLeft } from "fp-ts/lib/Either";
 import { getRequiredStringEnv } from "io-functions-commons/dist/src/utils/env";
 import { AzureContextTransport } from "io-functions-commons/dist/src/utils/logging";
 import * as t from "io-ts";
 import * as winston from "winston";
-import { appendSpidBlob } from "../utils/spid_blob_storage";
-
-const SPID_BLOB_STORAGE_CONNECTION_STRING = getRequiredStringEnv(
-  "QueueStorageConnection"
-);
 
 const SPID_BLOB_CONTAINER_NAME = getRequiredStringEnv(
   "SPID_BLOB_CONTAINER_NAME"
 );
 
-const blobService = createBlobService(SPID_BLOB_STORAGE_CONNECTION_STRING);
-
 /**
  * This type wraps a Spid Request/Response message sent over an azure storage queue, namely "spidmsgitems"
  */
 const SpidMsgItem = t.interface({
+  createdAt: t.string, // The timestamp of Request/Response creation
   ip: t.string, // The client ip that made a Spid login action
   payload: t.string, // The xml payload of a Spid Request/Response
-  timestamp: t.string // The timestamp of Request/Response creation
+  payloadType: t.string, // The information about payload type: REQUEST | RESPONSE
+  spidRequestId: t.string, // The SpiD unique identifier
+  today: t.string // The today's date with YYYY-MM-DD format
 });
 
 type SpidMsgItem = t.TypeOf<typeof SpidMsgItem>;
+
+/**
+ * This type wraps a Spid Blob item, stored in a Blob for each message
+ */
+const SpidBlobItem = t.interface({
+  createdAt: t.string, // The timestamp of Request/Response creation
+  ip: t.string, // The client ip that made a Spid login action
+  payload: t.string, // The xml payload of a Spid Request/Response
+  payloadType: t.string, // The nformation about payload type: REQUEST | RESPONSE
+  spidRequestId: t.string // The SpiD request ID
+});
+
+type SpidBlobItem = t.TypeOf<typeof SpidBlobItem>;
 
 // tslint:disable-next-line: no-let
 let logger: Context["log"] | undefined;
@@ -42,26 +49,24 @@ winston.add(contextTransport);
  * It handles call to utility that manages blob's related operations.
  * Store SPID request / responses, read from a queue, into a blob storage.
  */
-export function index(
+export async function index(
   context: Context,
   spidMsgItem: SpidMsgItem
-): Promise<azureStorage.BlobService.BlobResult> {
+): Promise<Error | void> {
   logger = context.log;
-  const today = format(new Date(), "YYYY-MM-DD");
-  return appendSpidBlob(
-    blobService,
-    SPID_BLOB_CONTAINER_NAME,
-    today,
-    JSON.stringify(spidMsgItem)
-  )
-    .fold(
-      l => {
-        throw l;
-      },
-      a => {
-        context.done();
-        return a;
-      }
-    )
-    .run();
+  const spidBlobItemOrError = SpidBlobItem.decode({
+    createdAt: spidMsgItem.createdAt,
+    ip: spidMsgItem.ip,
+    payload: spidMsgItem.payload,
+    payloadType: spidMsgItem.payloadType,
+    spidRequestId: spidMsgItem.spidRequestId
+  });
+  if (isLeft(spidBlobItemOrError)) {
+    return Promise.reject("Cannot decode Spid blob payload");
+  }
+  const spidBlobItem = spidBlobItemOrError.value;
+  // tslint:disable-next-line: no-object-mutation
+  context.bindings[SPID_BLOB_CONTAINER_NAME] = spidBlobItem;
+  context.done();
+  return Promise.resolve();
 }
