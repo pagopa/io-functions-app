5095524e1f67bd6a44d3f08bc777b8682bc6f824
Danilo Spinelli <gunzip@users.noreply.github.com>
2020-04-20 22:39:34 +0200

    [#172405623] fix typo in welcome message (#70)

--- SendWelcomeMessagesActivity/index.ts
+++ SendWelcomeMessagesActivity/index.ts
@@ -42,7 +42,7 @@ All’arrivo di ogni nuovo messaggio, IO ti informa attraverso una notifica e un
 
 #### I pagamenti
 
-IO integra [pagoPA](https://www.pagopa.gov.it/) per effettuare pagamenti sicuri verso la Pubblica Amministrazione. Se hai già usato pagoPA come utente registrato, nella sezione [Pagamenti](ioit://WALLET_HOME) potrai vedere lo storico delle transazioni effettuate e le tue carte di credito salvate. Puoi usare la sezione [Pagamenti](ioit://WALLET_HOME) per aggiungere i tuoi metodi di pagmaento preferiti, procedere al pagamento di un avviso pagoPA (tramite scansione del codice QR o inserimento manuale del codice IUV) e tenere traccia di tutte le operazioni svolte.
+IO integra [pagoPA](https://www.pagopa.gov.it/) per effettuare pagamenti sicuri verso la Pubblica Amministrazione. Se hai già usato pagoPA come utente registrato, nella sezione [Pagamenti](ioit://WALLET_HOME) potrai vedere lo storico delle transazioni effettuate e le tue carte di credito salvate. Puoi usare la sezione [Pagamenti](ioit://WALLET_HOME) per aggiungere i tuoi metodi di pagamento preferiti, procedere al pagamento di un avviso pagoPA (tramite scansione del codice QR o inserimento manuale del codice IUV) e tenere traccia di tutte le operazioni svolte.
 
 #### I servizi
 
