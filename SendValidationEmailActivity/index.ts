58435e6300a814e23cf7b2eb2eb4ff85c6cca49f
Simone Infante <simone.infante@pagopa.it>
2020-11-04 18:28:22 +0100

    Add mail common module

--- SendValidationEmailActivity/index.ts
+++ SendValidationEmailActivity/index.ts
@@ -1,33 +1,12 @@
-﻿import { MailMultiTransportConnectionsFromString } from "io-functions-commons/dist/src/utils/multi_transport_connection";
-import { MultiTransport } from "io-functions-commons/dist/src/utils/nodemailer";
-import { NonEmptyString } from "italia-ts-commons/lib/strings";
-
-import * as NodeMailer from "nodemailer";
-
-import { getSendValidationEmailActivityHandler } from "./handler";
-
-import {
-  getMailerTransporter,
-  getTransportsForConnections
-} from "../utils/email";
+﻿import { getSendValidationEmailActivityHandler } from "./handler";
 
 import { initTelemetryClient } from "../utils/appinsights";
 
 import { getConfigOrThrow } from "../utils/config";
 
+import { getMailerTransporter } from "io-functions-commons/dist/src/mailer";
+
 const config = getConfigOrThrow();
-
-// Whether we're in a production environment
-const isProduction = config.NODE_ENV === "production";
-
-// Optional SendGrid key
-const sendgridApiKey = NonEmptyString.decode(config.SENDGRID_API_KEY).getOrElse(
-  undefined
-);
-
-// Mailup
-const mailupUsername = config.MAILUP_USERNAME;
-const mailupSecret = config.MAILUP_SECRET;
 
 // Email data
 const EMAIL_TITLE = "Valida l’indirizzo email che usi su IO";
@@ -45,31 +24,7 @@ const emailDefaults = {
 
 export type EmailDefaults = typeof emailDefaults;
 
-// Optional multi provider connection string
-// The connection string must be in the format:
-//   [mailup:username:password;][sendgrid:apikey:;]
-// Note that multiple instances of the same provider can be provided.
-const transports = MailMultiTransportConnectionsFromString.decode(
-  config.MAIL_TRANSPORTS
-)
-  .map(getTransportsForConnections)
-  .getOrElse([]);
-
-// if we have a valid multi transport configuration, configure a
-// Multi transport, or else fall back to the default logic
-const mailerTransporter =
-  transports.length > 0
-    ? NodeMailer.createTransport(
-        MultiTransport({
-          transports
-        })
-      )
-    : getMailerTransporter({
-        isProduction,
-        ...(sendgridApiKey
-          ? { sendgridApiKey }
-          : { mailupSecret, mailupUsername })
-      });
+const mailerTransporter = getMailerTransporter(config);
 
 // Initialize application insights
 initTelemetryClient();
