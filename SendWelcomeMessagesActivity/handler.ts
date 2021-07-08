c1c11790a475faba6805fcc0c744d50c9e79bdb9
Jacopo Pompilii <Jacopo.pompilii@pagopa.it>
2021-07-08 17:51:42 +0200

    Update handler.ts
    - Updated welcome messages
    - Removed references to cashback
    - Updated info about Services

--- SendWelcomeMessagesActivity/handler.ts
+++ SendWelcomeMessagesActivity/handler.ts
@@ -1,4 +1,4 @@
-﻿import { AzureFunction, Context } from "@azure/functions";
+import { AzureFunction, Context } from "@azure/functions";
 
 import { toString } from "fp-ts/lib/function";
 import * as t from "io-ts";
@@ -31,47 +31,34 @@ const welcomeMessages: WelcomeMessages = {
   WELCOME: (_: RetrievedProfile) =>
     NewMessage.decode({
       content: {
-        markdown: `## Benvenuto nella prima versione pubblica di IO!
+        markdown: `Ciao! IO consente agli enti pubblici di portare progressivamente i propri servizi di comunicazione, pagamento, welfare o documentazione in un'unica app, e ai cittadini italiani di avere dei servizi pubblici nuovi, moderni e centrati sulle proprie esigenze e di interagire con le Pubbliche Amministrazioni in modo sicuro e sempre a portata di mano. 
 
-Ciao! Questa è la prima versione di IO, l’app dei servizi pubblici, aperta a tutti i cittadini. Cosa vuol dire? Significa che, come te, chiunque lo desideri potrà installare e iniziare a usare l’app, sapendo che evolverà e migliorerà continuamente anche grazie all’esperienza diretta e ai feedback delle persone a cui è destinata.
-
-Rendendo IO disponibile per tutti, consentiamo agli enti pubblici di portare progressivamente i propri servizi sull’app e ai cittadini italiani di avere dei servizi pubblici nuovi, moderni e centrati sulle proprie esigenze.
-
-### Scopri le funzioni di IO
-
-Ecco come potrai usare IO attraverso le diverse funzioni disponibili oggi nell’app: molte altre ne arriveranno nei prossimi mesi!  
-IO ti permette di richiedere e spendere il “[Bonus Vacanze](https://io.italia.it/bonus-vacanze/)” e a breve anche di partecipare al Bonus Cashback, in arrivo nella sezione “Portafoglio” dell’app. IO ti avviserà con un aggiornamento quando il nuovo servizio sarà disponibile.
+#### Le funzioni di IO
+Ecco come potrai usare IO attraverso le diverse funzioni disponibili oggi nell’app:   
 
 #### I messaggi
-
-IO ti consente di ricevere comunicazioni da tutte le Pubbliche Amministrazioni italiane, locali e nazionali, di tenere sotto controllo ogni scadenza e, all’occorrenza, effettuare dei pagamenti.
-All’arrivo di ogni nuovo messaggio, IO ti informa attraverso una notifica e un inoltro del messaggio sulla tua casella di posta: nella sezione [servizi](ioit://SERVICES_HOME) puoi decidere come essere contattato da ciascun servizio.
+IO ti consente di ricevere comunicazioni da tutte le Pubbliche Amministrazioni italiane, locali e nazionali, di tenere sotto controllo ogni scadenza e, all’occorrenza, effettuare dei pagamenti. 
+All’arrivo di ogni nuovo messaggio, a seconda delle tue preferenze, IO può informarti attraverso una notifica e inoltrarti il messaggio sulla tua casella di posta: nel dettaglio dei servizi, o nelle preferenze presenti nel Profilo, puoi decidere come ricevere comunicazioni da ciascun servizio. 
 
 #### I pagamenti
-
-IO integra [pagoPA](https://www.pagopa.gov.it/) per effettuare pagamenti sicuri verso la Pubblica Amministrazione. Se hai già usato pagoPA come utente registrato, nella sezione [Portafoglio](ioit://WALLET_HOME) potrai vedere lo storico delle transazioni effettuate e le tue carte di credito salvate. Puoi usare la sezione [Portafoglio](ioit://WALLET_HOME) per aggiungere i tuoi metodi di pagamento preferiti, procedere al pagamento di un avviso pagoPA (tramite scansione del codice QR o inserimento manuale del codice IUV) e tenere traccia di tutte le operazioni svolte.
-Nella sezione [Portafoglio](ioit://WALLET_HOME) troverai la possibilità di richiedere il [Bonus Vacanze](https://io.italia.it/bonus-vacanze/) e il Bonus Cashback, disponibile a partire dal 1° dicembre.
+IO integra [pagoPA](https://pagopa.gov.it) per effettuare pagamenti sicuri verso la Pubblica Amministrazione. Dalla sezione Portafoglio puoi aggiungere i tuoi metodi di pagamento preferiti, procedere al pagamento di un avviso pagoPA (tramite scansione del codice QR o inserimento manuale del codice avviso) e tenere traccia di tutte le operazioni svolte. Se hai già usato pagoPA con SPID in passato, nella sezione Portafoglio potrai vedere lo storico delle transazioni effettuate e i tuoi metodi di pagamento salvati. 
 
 #### I servizi
+Al primo accesso in IO, IO ti chiede di impostare una configurazione generale per la comunicazione dei servizi. Nella sezione Servizi puoi vedere la lista di tutti i servizi locali e nazionali disponibili all’interno dell’app, e decidere da quali ricevere comunicazioni e come. 
 
-Al momento del tuo primo accesso in IO, tutti i Servizi disponibili sono attivi. Nella sezione [servizi](ioit://SERVICES_HOME) puoi vedere quali sono i servizi locali e nazionali disponibili all’interno dell’app, e decidere da quali essere contattato e come. Aggiungi le tue aree geografiche di interesse per essere sempre informato sui nuovi servizi in arrivo nel tuo Comune o nella tua Regione.
-
-#### Serve aiuto ?
-
-Se qualcosa non ti è chiaro durante l’utilizzo dell’app, usa il punto di domanda in alto a destra per leggere approfondimenti e indicazioni utili relative alla sezione in cui ti trovi.
-Ricorda che i messaggi che IO ti recapita sono inviati direttamente dagli enti pubblici: se i tuoi dubbi riguardano il contenuto di un messaggio, contatta direttamente l’ente che ti ha scritto con gli appositi pulsanti che trovi nel corpo del messaggio o nella scheda del servizio.
-Se hai un problema e non trovi risposte utili nella pagina di aiuto, contatta il team di IO utilizzando il pulsante con la coccinella (se ti sembra si tratti di un malfunzionamento) o l’icona della chat (per interagire direttamente con la nostra assistenza, in caso di problemi urgenti).
+#### Serve aiuto?
+Se qualcosa non ti è chiaro durante l’utilizzo dell’app, usa il punto di domanda in alto a destra per leggere approfondimenti e indicazioni utili relative alla schermata in cui ti trovi.
+Ricorda che i messaggi che IO ti recapita sono inviati direttamente dagli enti pubblici: se i tuoi dubbi riguardano il contenuto di un messaggio, segnalalo direttamente all’ente che ti ha scritto; trovi i contatti nel corpo del messaggio o nella scheda del servizio. 
+Se hai un problema e non trovi risposte utili nella pagina di aiuto, contatta il team di IO utilizzando il pulsante con la coccinella “Scrivi al team di IO”.
 
 #### Come partecipare al progetto
-
-IO è un progetto che cresce e migliora giorno dopo giorno grazie al contributo degli utenti. Per questo ti chiediamo, se vorrai, di segnalarci ogni aspetto dal tuo punto di vista migliorabile, cliccando sul bottone Scrivi al Team di IO con l’icona a coccinella (per segnalare errori di funzionamento) o la chat (per inviare suggerimenti o commentare la tua esperienza).
-
-IO è un progetto 100% open source. Se sei un designer o uno sviluppatore, puoi contribuire direttamente al design e sviluppo dell’applicazione: visita il sito [io.italia.it](https://io.italia.it) per trovare più informazioni su come dare il tuo contributo.
+IO è un progetto che cresce e migliora giorno dopo giorno grazie al contributo degli utenti. Per questo ti chiediamo, se vorrai, di segnalarci eventuali feedback, richieste di migliorie o di nuove funzionalità. 
+IO è un progetto open source. Se ti occupi di design o sviluppo, puoi contribuire direttamente: visita il sito [io.italia.it](https://io.italia.it) per maggiori informazioni.
 
 Grazie di far parte del progetto IO!
 `,
 
-        subject: `Benvenuto su IO`
+        subject: `Ti diamo il benvenuto in IO`
       }
     }).getOrElseL(throwInvalidMessageError),
   // tslint:disable-next-line: object-literal-sort-keys
@@ -80,23 +67,18 @@ Grazie di far parte del progetto IO!
       content: {
         markdown: `L’app IO nasce per offrirti un unico punto di accesso, comodo, semplice e sicuro, verso tutti i servizi della Pubblica Amministrazione.
 
-Dall’8 dicembre 2020 parte il programma Cashback, con l’Extra Cashback di Natale. Con l’app IO potrai aderire al programma tramite la relativa scheda di servizio o dalla sezione **Portafoglio** dell’app: puoi ottenere un rimborso sugli acquisti effettuati con carte, bancomat e app di pagamento. Il “Cashback” è una delle iniziative del Piano Italia Cashless promosso dal Governo allo scopo di incentivare un maggiore utilizzo di moneta elettronica nel Paese.
+Oltre 13.000 tra enti nazionali e locali hanno già portato a bordo i loro servizi – relativi, ad esempio, ad anagrafe, scuola, tributi, mobilità, attività produttive, edilizia e sanità – e molti se ne aggiungeranno in futuro: visualizza l’elenco completo di quelli disponibili nella sezione **[Servizi](ioit://SERVICES_HOME)**.
 
-Oltre al Cashback, promosso dal Ministero dell’Economia e delle Finanze, diversi enti nazionali e locali hanno già portato i loro servizi a bordo e molti se ne aggiungeranno in futuro. Nella sezione **[Servizi](ioit://SERVICES_HOME)** puoi indicare le aree geografiche di tuo interesse (dove vivi, dove lavori, o dove magari vai spesso) per ricevere aggiornamenti sui nuovi servizi in arrivo in quel Comune o in quella Regione.
+Se non vedi gli enti del tuo territorio tra quelli elencati, è perché i loro servizi sono in corso di integrazione o arriveranno prossimamente.
+Puoi decidere tu quali servizi possono contattarti, ma ti consigliamo di lasciare la comunicazione sempre accesa (impostando la configurazione rapida nelle preferenze) perché un servizio ti scriverà solo quando avrà qualcosa di importante da dire proprio a te!  Ad esempio: per la scadenza di un documento, il promemoria per un pagamento o l’aggiornamento su una pratica in corso.
 
-Se non vedi gli enti del tuo territorio tra quelli elencati, è perché i loro servizi sono ancora in corso di integrazione. Se vuoi saperne di più, chiedi al tuo Comune se hanno attivato il processo per essere presenti su IO e a che punto sono. La tua voce può essere un segnale importante!
-
-Tutti i servizi all’interno dell’app sono attivi: questo significa che ti scriveranno quando hanno qualcosa di rilevante da dire proprio a te, ad esempio: la scadenza di un documento, il promemoria per un pagamento o l’aggiornamento su una pratica in corso.
-Puoi disattivare ciascun servizio in qualsiasi momento: in quel caso, l’Ente continuerà a contattarti avvalendosi dei canali tradizionali (come ad esempio la posta cartacea).
-In particolare:
-- nella sezione [Servizi](ioit://SERVICES_HOME) puoi disattivare un singolo servizio, tutti quelli di un singolo Ente o tutti i servizi in generale. Inoltre, dalla scheda di ciascun servizio, potrai disattivare le notifiche via email e/o push;
-- nella Sezione [Preferenze](ioit://PROFILE_PREFERENCES_HOME) troverai la funzione "Inoltro dei messaggi via email" dove potrai abilitare/disabilitare tale modalità di notifica per tutti o singoli servizi.
+Utilizzando la configurazione manuale dovrai attivare uno a uno i servizi che ti interessano. Nelle preferenze potrai anche decidere di attivare la funzione di inoltro dei messaggi che riceverai su IO, anche al tuo indirizzo e-mail. 
 
 Infine, è importante sapere che per ora i messaggi inviati dagli enti tramite IO non hanno valore legale.
 
 Per maggiori informazioni sull’avanzamento del progetto, visita il sito [io.italia.it](https://io.italia.it).
 `,
-        subject: `Il Cashback e gli altri servizi su IO`
+        subject: `Quali servizi trovi su IO?`
       }
     }).getOrElseL(throwInvalidMessageError),
   CASHBACK: (_: RetrievedProfile) =>
