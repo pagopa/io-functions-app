d018730e71d922b67ea77336f1d032abdd35cd53
Michael Disaro <michael.disaro@gmail.com>
2021-09-15 17:51:48 +0200

    new pagination handler and models

--- GetMessages/handler.ts
+++ GetMessages/handler.ts
@@ -1,28 +1,23 @@
-import { mapAsyncIterator } from "@pagopa/io-functions-commons/dist/src/utils/async";
+import {
+  asyncIteratorToPageArray,
+  mapAsyncIterator,
+  flattenAsyncIterator
+} from "@pagopa/io-functions-commons/dist/src/utils/async";
 import { retrievedMessageToPublic } from "@pagopa/io-functions-commons/dist/src/utils/messages";
 import { FiscalCodeMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/fiscalcode";
 import {
   withRequestMiddlewares,
   wrapRequestHandler
 } from "@pagopa/io-functions-commons/dist/src/utils/request_middleware";
-import {
-  IResponseErrorQuery,
-  IResponseSuccessPageIdBasedIterator
-} from "@pagopa/io-functions-commons/dist/src/utils/response";
-
-import { flattenAsyncIterator } from "@pagopa/io-functions-commons/dist/src/utils/async";
-
+import { IResponseErrorQuery } from "@pagopa/io-functions-commons/dist/src/utils/response";
+import { PageResults, toPageResults } from "@pagopa/io-functions-commons/dist/src/utils/paging";
 import {
   defaultPageSize,
   MessageModel,
   RetrievedMessage
 } from "@pagopa/io-functions-commons/dist/src/models/message";
-
 import { CreatedMessageWithoutContent } from "@pagopa/io-functions-commons/dist/generated/definitions/CreatedMessageWithoutContent";
-import { EnrichedMessage } from "@pagopa/io-functions-commons/dist/generated/definitions/EnrichedMessage";
-
 import { OptionalQueryParamMiddleware } from "@pagopa/io-functions-commons/dist/src/utils/middlewares/optional_query_param";
-
 import * as express from "express";
 import * as A from "fp-ts/lib/Array";
 import * as E from "fp-ts/lib/Either";
@@ -30,7 +25,6 @@ import { pipe } from "fp-ts/lib/function";
 import * as T from "fp-ts/lib/Task";
 import * as TE from "fp-ts/lib/TaskEither";
 import * as t from "io-ts";
-
 import { ServiceModel } from "@pagopa/io-functions-commons/dist/src/models/service";
 import { BooleanFromString } from "@pagopa/ts-commons/lib/booleans";
 import {
@@ -56,7 +50,7 @@ const RetrievedNotPendingMessage = t.intersection([
 ]);
 
 type IGetMessagesHandlerResponse =
-  | IResponseSuccessJson<{}>
+  | IResponseSuccessJson<PageResults>
   | IResponseErrorInternal
   | IResponseErrorValidation
   | IResponseErrorQuery;
@@ -134,7 +128,7 @@ export const GetMessagesHandler = (
         ),
         TE.map(flattenAsyncIterator),
         TE.chain(i =>
-          TE.tryCatch(() => asyncIteratorToPage(i, pageSize), E.toError)
+          TE.tryCatch(() => asyncIteratorToPageArray(i, pageSize), E.toError)
         ),
         TE.chain(
           TE.fromPredicate(
@@ -145,67 +139,11 @@ export const GetMessagesHandler = (
         TE.map(({ hasMoreResults, results }) =>
           toPageResults(A.rights([...results]), hasMoreResults)
         ),
-        TE.bimap(
-          e => ResponseErrorInternal(e.message),
-          ResponseSuccessJson
-        ),
+        TE.bimap(e => ResponseErrorInternal(e.message), ResponseSuccessJson),
         TE.toUnion
       )()
     )
   )();
-
-interface IPage<T> {
-  results: ReadonlyArray<T>;
-  hasMoreResults: boolean;
-}
-
-export const asyncIteratorToPage = async <T>(
-  iter: AsyncIterator<T | Promise<T>>,
-  pageSize: NonNegativeInteger
-): Promise<IPage<T>> => {
-  const acc = Array<T>();
-  // tslint:disable-next-line: no-let
-  let hasMoreResults: boolean = true;
-
-  // eslint-disable-next-line no-constant-condition
-  while (true) {
-    const next = await iter.next();
-    if (next.done === true) {
-      hasMoreResults = false;
-      break;
-    }
-    if (acc.length === pageSize) {
-      break;
-    }
-    // eslint-disable-next-line functional/immutable-data
-    acc.push(await next.value);
-  }
-
-  return { results: acc, hasMoreResults };
-};
-
-const toPageResults = <T extends { readonly id: string }>(
-  items: ReadonlyArray<T>,
-  hasMoreResults: boolean
-) => {
-  const next = hasMoreResults
-    ? pipe(
-        O.fromNullable(items[items.length - 1]),
-        O.map(e => e.id),
-        O.toUndefined
-      )
-    : undefined;
-  const prev = pipe(
-    O.fromNullable(items[0]),
-    O.map(e => e.id),
-    O.toUndefined
-  );
-  return {
-    items,
-    next,
-    prev
-  };
-};
 
 /**
  * Wraps a GetMessages handler inside an Express request handler.
