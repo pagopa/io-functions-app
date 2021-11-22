5bb208e5affd8caf7fd5de225a5a07f04cdd81a8
AleDore <alessio85.dore@gmail.com>
2021-11-22 12:53:10 +0100

    enrich Message with category

--- GetMessages/__tests__/handler.test.ts
+++ GetMessages/__tests__/handler.test.ts
@@ -23,6 +23,7 @@ import { BlobService } from "azure-storage";
 import { ServiceModel } from "@pagopa/io-functions-commons/dist/src/models/service";
 import { toCosmosErrorResponse } from "@pagopa/io-functions-commons/dist/src/utils/cosmosdb_model";
 import { Context } from "@azure/functions";
+import { MessageCategoryEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/MessageCategory";
 
 const aFiscalCode = "FRLFRC74E04B157I" as FiscalCode;
 const aMessageId = "A_MESSAGE_ID" as NonEmptyString;
@@ -384,6 +385,7 @@ describe("GetMessagesHandler", () => {
 
     const expectedEnrichedMessage = {
       ...retrievedMessageToPublic(aRetrievedMessageWithoutContent),
+      category: MessageCategoryEnum.GENERIC,
       message_title: "a subject",
       organization_name: aRetrievedService.organizationName,
       service_name: aRetrievedService.serviceName
