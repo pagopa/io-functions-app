095144813a96e366e8d51a4ec10eb1d1fc6aff7e
fabriziopapi <fabrizio.papi@pagopa.it>
2021-12-16 10:49:28 +0100

    [#IC-68] [#IC-68] (+) update message model (+) update service metatdata to resolve new commons version errors

--- GetVisibleServices/__tests__/handler.test.ts
+++ GetVisibleServices/__tests__/handler.test.ts
@@ -28,6 +28,10 @@ import { ServiceScopeEnum } from "@pagopa/io-functions-commons/dist/generated/de
 import { IResponseSuccessJson } from "@pagopa/ts-commons/lib/responses";
 import { aCosmosResourceMetadata } from "../../__mocks__/mocks";
 import { GetVisibleServices, GetVisibleServicesHandler } from "../handler";
+import { StandardServiceCategoryEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/StandardServiceCategory";
+import { SpecialServiceCategoryEnum } from "@pagopa/io-functions-commons/dist/generated/definitions/SpecialServiceCategory";
+
+import * as E from "fp-ts/Either";
 
 afterEach(() => {
   jest.resetAllMocks();
@@ -74,12 +78,16 @@ const aVisibleService: VisibleService = {
   version: aRetrievedService.version
 };
 
-const aLocalVisibleService: VisibleService = {
-  ...aVisibleService,
-  serviceMetadata: {
-    scope: ServiceScopeEnum.LOCAL
-  }
-};
+const aLocalVisibleService: VisibleService = E.getOrElseW(() => {
+  throw new Error("wrong dummy visible service ");
+})(
+  VisibleService.decode({
+    ...aVisibleService,
+    serviceMetadata: {
+      scope: ServiceScopeEnum.LOCAL
+    }
+  })
+);
 
 describe("GetVisibleServicesHandler", () => {
   it("should get all visible services", async () => {
