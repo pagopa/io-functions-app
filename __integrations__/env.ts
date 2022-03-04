63027cc9a8fd1d45df611156f2a343d6202fb12c
gquadrati <greta.quadrati@pagopa.it>
2022-03-04 13:42:39 +0100

    [#IC-333] setup integration tests

---
+++ __integrations__/env.ts
@@ -0,0 +1,12 @@
+export const AzureWebJobsStorage = process.env.AzureWebJobsStorage;
+export const QueueStorageConnection = process.env.QueueStorageConnection || "";
+
+// Milliseconds to wait for test completion
+export const WAIT_MS = Number(process.env.WAIT_MS ?? 5000);
+export const SHOW_LOGS = process.env.SHOW_LOGS === "true";
+
+export const COSMOSDB_URI = process.env.COSMOSDB_URI;
+export const COSMOSDB_KEY = process.env.COSMOSDB_KEY;
+export const COSMOSDB_NAME = process.env.COSMOSDB_NAME;
+
+export const MESSAGE_CONTAINER_NAME = process.env.MESSAGE_CONTAINER_NAME;
