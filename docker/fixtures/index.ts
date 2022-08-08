47d35cb4d8e8cce3e5ffbe9b61f656abe20f68f1
Gabriele Mendolia <arcogabbo@gmail.com>
2022-08-08 15:33:47 +0200

    [#IOCIT-29] porting docker/fixtures/index.ts file to fp-ts 2 (#257)

--- docker/fixtures/index.ts
+++ docker/fixtures/index.ts
@@ -14,11 +14,11 @@ import {
   SERVICE_COLLECTION_NAME,
   ServiceModel
 } from "@pagopa/io-functions-commons/dist/src/models/service";
-
 import { sequenceT } from "fp-ts/lib/Apply";
 import { toError } from "fp-ts/lib/Either";
-import { TaskEither, taskEitherSeq, tryCatch } from "fp-ts/lib/TaskEither";
-
+import * as TE from "fp-ts/lib/TaskEither";
+import * as E from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/function";
 import { getConfigOrThrow } from "../../utils/config";
 
 const config = getConfigOrThrow();
@@ -31,90 +31,103 @@ export const cosmosdbClient = new CosmosClient({
   key: cosmosDbKey
 });
 
-function createDatabase(databaseName: string): TaskEither<Error, Database> {
-  return tryCatch(
-    () => cosmosdbClient.databases.create({ id: databaseName }),
-    toError
-  ).map(_ => _.database);
-}
+const createDatabase = (databaseName: string): TE.TaskEither<Error, Database> =>
+  pipe(
+    TE.tryCatch(
+      () => cosmosdbClient.databases.create({ id: databaseName }),
+      toError
+    ),
+    TE.map(response => response.database)
+  )
 
-function createCollection(
+const createCollection = (
   db: Database,
   collectionName: string,
   partitionKey: string
-): TaskEither<Error, ContainerResponse> {
-  return tryCatch(
+): TE.TaskEither<Error, ContainerResponse> =>
+  TE.tryCatch(
     () => db.containers.createIfNotExists({ id: collectionName, partitionKey }),
     toError
   );
-}
 
-const aService: Service = Service.decode({
-  authorizedCIDRs: [],
-  authorizedRecipients: [],
-  departmentName: "Deparment Name",
-  isVisible: true,
-  maxAllowedPaymentAmount: 100000,
-  organizationFiscalCode: "01234567890",
-  organizationName: "Organization name",
-  requireSecureChannels: false,
-  serviceId: config.REQ_SERVICE_ID,
-  serviceName: "MyServiceName"
-}).getOrElseL(() => {
-  throw new Error("Cannot decode service payload.");
-});
+const aService: Service = pipe(
+  Service.decode({
+    authorizedCIDRs: [],
+    authorizedRecipients: [],
+    departmentName: "Deparment Name",
+    isVisible: true,
+    maxAllowedPaymentAmount: 100000,
+    organizationFiscalCode: "01234567890",
+    organizationName: "Organization name",
+    requireSecureChannels: false,
+    serviceId: config.REQ_SERVICE_ID,
+    serviceName: "MyServiceName"
+  }),
+  E.getOrElse(() => {
+    throw new Error("Cannot decode service payload.");
+  })
+)
 
-const aNewService = NewService.decode({
-  ...aService,
-  kind: "INewService"
-}).getOrElseL(() => {
-  throw new Error("Cannot decode new service.");
-});
+const aNewService = pipe(
+  NewService.decode({
+    ...aService,
+    kind: "INewService"
+  }),
+  E.getOrElse(() => {
+    throw new Error("Cannot decode new service.");
+  }))
 
-const aProfile: Profile = Profile.decode({
-  acceptedTosVersion: 1,
-  email: "email@example.com",
-  fiscalCode: "AAAAAA00A00A000A",
-  isEmailEnabled: true,
-  isEmailValidated: true,
-  isInboxEnabled: true,
-  isWebhookEnabled: true
-}).getOrElseL(() => {
-  throw new Error("Cannot decode profile payload.");
-});
+const aProfile: Profile = pipe(
+  Profile.decode({
+    acceptedTosVersion: 1,
+    email: "email@example.com",
+    fiscalCode: "AAAAAA00A00A000A",
+    isEmailEnabled: true,
+    isEmailValidated: true,
+    isInboxEnabled: true,
+    isWebhookEnabled: true
+  }),
+  E.getOrElse(() => {
+    throw new Error("Cannot decode profile payload.");
+  }))
 
-const aNewProfile = NewProfile.decode({
-  ...aProfile,
-  kind: "INewProfile"
-}).getOrElseL(() => {
-  throw new Error("Cannot decode new profile.");
-});
+const aNewProfile = pipe(
+  NewProfile.decode({
+    ...aProfile,
+    kind: "INewProfile"
+  }), E.getOrElse(() => {
+    throw new Error("Cannot decode new profile.");
+  })
+)
 
-createDatabase(config.COSMOSDB_NAME)
-  .chain(db =>
-    sequenceT(taskEitherSeq)(
+pipe(
+  createDatabase(config.COSMOSDB_NAME),
+  TE.chainFirst(db =>
+    sequenceT(TE.ApplySeq)(
       createCollection(db, "message-status", "messageId"),
       createCollection(db, "messages", "fiscalCode"),
       createCollection(db, "notification-status", "notificationId"),
       createCollection(db, "notifications", "messageId"),
       createCollection(db, "profiles", "fiscalCode"),
       createCollection(db, "services", "serviceId")
-    ).map(_ => db)
-  )
-  .chain(db =>
-    sequenceT(taskEitherSeq)(
-      new ServiceModel(db.container(SERVICE_COLLECTION_NAME)).create(
-        aNewService
+    )
+  ),
+  TE.chain(db =>
+    pipe(
+      sequenceT(TE.ApplySeq)(
+        new ServiceModel(db.container(SERVICE_COLLECTION_NAME)).create(
+          aNewService
+        ),
+        new ProfileModel(db.container(PROFILE_COLLECTION_NAME)).create(
+          aNewProfile
+        )
       ),
-      new ProfileModel(db.container(PROFILE_COLLECTION_NAME)).create(
-        aNewProfile
-      )
-    ).mapLeft(_ => new Error(`CosmosError: ${_.kind}`))
+      TE.mapLeft(_ => new Error(`CosmosError: ${_.kind}`))
+    )
   )
-  .run()
-  .then(
-    // eslint-disable-next-line no-console
-    _ => console.log(`Successfully created fixtures`),
-    // eslint-disable-next-line no-console
-    _ => console.error(`Failed generate fixtures ${_.message}`)
-  );
+)().then(
+  // eslint-disable-next-line no-console
+  _ => console.log(`Successfully created fixtures`),
+  // eslint-disable-next-line no-console
+  _ => console.error(`Failed generate fixtures ${_.message}`)
+)
