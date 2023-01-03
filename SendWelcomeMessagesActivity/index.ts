6a0fadd4eed17f8a5d1ae33122a3ec905d1e8440
Alessio Dore <57567806+AleDore@users.noreply.github.com>
2023-01-03 15:42:43 +0100

    Fix Fetch with Both HTTP or HTTPS support (#272)

--- SendWelcomeMessagesActivity/index.ts
+++ SendWelcomeMessagesActivity/index.ts
@@ -1,4 +1,4 @@
-import { agent } from "@pagopa/ts-commons";
+import { getFetch } from "@pagopa/ts-commons/lib/agent";
 import {
   AbortableFetch,
   setFetchTimeout,
@@ -13,12 +13,12 @@ const DEFAULT_REQUEST_TIMEOUT_MS = 10000;
 
 const config = getConfigOrThrow();
 
-// HTTP-only fetch with optional keepalive agent
+// Generic HTTP/HTTPS fetch with optional keepalive agent
 // @see https://github.com/pagopa/io-ts-commons/blob/master/src/agent.ts#L10
-const httpApiFetch = agent.getHttpFetch(process.env);
+const httpOrHttpsApiFetch = getFetch(process.env);
 
 // a fetch that can be aborted and that gets cancelled after fetchTimeoutMs
-const abortableFetch = AbortableFetch(httpApiFetch);
+const abortableFetch = AbortableFetch(httpOrHttpsApiFetch);
 const timeoutFetch = toFetch(
   setFetchTimeout(DEFAULT_REQUEST_TIMEOUT_MS as Millisecond, abortableFetch)
 );
