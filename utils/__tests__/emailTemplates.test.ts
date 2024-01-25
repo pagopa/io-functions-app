19d377df967fb30ae521dc52e7e729c3946bf593
arcogabbo <arcogabbo@gmail.com>
2024-01-25 16:01:20 +0100

    [#IOPID-1326] updated templated validation email

--- utils/__tests__/emailTemplates.test.ts
+++ utils/__tests__/emailTemplates.test.ts
@@ -1,10 +1,11 @@
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import { apply as loginEmailApply } from "../../generated/templates/login/index";
 import { apply as fallbackLoginEmailApply } from "../../generated/templates/login-fallback/index";
+import { apply as validationEmailApply } from "../../generated/templates/mailvalidation/index";
 import { ValidUrl } from "@pagopa/ts-commons/lib/url";
 
 describe("Email Templates", () => {
-  it("should generate login notification email", async () => {
+  it("should generate login notification email", () => {
     const result = loginEmailApply(
       "Mario" as NonEmptyString,
       "idp-prova" as NonEmptyString,
@@ -16,7 +17,7 @@ describe("Email Templates", () => {
     expect(result).toMatchSnapshot();
   });
 
-  it("should generate fallback login notification email", async () => {
+  it("should generate fallback login notification email", () => {
     const anAccessUrl = { href: "https://website.it" } as ValidUrl;
     const result = fallbackLoginEmailApply(
       "Mario" as NonEmptyString,
@@ -26,6 +27,14 @@ describe("Email Templates", () => {
       anAccessUrl
     );
 
+    expect(result).toMatchSnapshot();
+  });
+
+  it("should generate validation email", () => {
+    const aTitle = "MY TITLE";
+    const aValidationUrl = "https://example.com?result=success";
+
+    const result = validationEmailApply(aTitle, aValidationUrl);
     expect(result).toMatchSnapshot();
   });
 });
