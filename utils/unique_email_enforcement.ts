bcd15b5a1cea230f09fc605d1d584b94b4fe2f5c
Salvatore Laiso <salvatore.laiso@it.ey.com>
2024-10-03 14:44:02 +0200

    refactor: remove `FF_UNIQUE_EMAIL_ENFORCEMENT_ENABLED`

--- utils/unique_email_enforcement.ts
+++ utils/unique_email_enforcement.ts
@@ -10,11 +10,3 @@ export const profileEmailTableClient = TableClient.fromConnectionString(
   config.PROFILE_EMAIL_STORAGE_CONNECTION_STRING,
   config.PROFILE_EMAIL_STORAGE_TABLE_NAME
 );
-
-export const FF_UNIQUE_EMAIL_ENFORCEMENT_ENABLED = getIsUserEligibleForNewFeature<
-  FiscalCode
->(
-  fiscalCode => config.UNIQUE_EMAIL_ENFORCEMENT_USERS.includes(fiscalCode),
-  () => false,
-  config.FF_UNIQUE_EMAIL_ENFORCEMENT
-);
